814b67e7c4742885bb34f75c36c8733048ce7695
Yating <151861203+28TheCat@users.noreply.github.com>
2025-11-17 21:24:06 +0800

    Add JUnit dependency and update test code
    Added JUnit 4.13.2 as a test dependency in pom.xml. Updated Main.java with a sample modulo operation and replaced Solution.java with a new implementation for finding the intersection node of two linked lists. Also added and updated several files for Python Titanic data analysis projects, including notebooks, data files, and project configuration.

--- Java/demo/src/test/java/Main.java
+++ Java/demo/src/test/java/Main.java
@@ -2,5 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        int a=7;
+        System.out.println(a%4);
     }
 }
--- Java/demo/src/test/java/Solution.java
+++ Java/demo/src/test/java/Solution.java
@@ -1,10 +1,24 @@
-class Solution {
-    public String toLowerCase(String s) {
-        for (int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            if (c >= 'A' && c <= 'Z'){
-                s.charAt( i) -=32 ;
-            }
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null)return null;
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a != b){
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+            if (a == b)return a;
         }
+        return null;
     }
 }
